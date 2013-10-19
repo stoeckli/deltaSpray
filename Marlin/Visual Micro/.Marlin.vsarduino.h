02215f1008143ecb109c8680f7699be9e55ba5bd
stoeckli <email@stoeckli.net>
2013-10-19 18:21:34 +0200

    ident changes

--- Marlin/Visual Micro/.Marlin.vsarduino.h
+++ Marlin/Visual Micro/.Marlin.vsarduino.h
@@ -2,11 +2,12 @@
 #define _VSARDUINO_H_
 //Board = Arduino Mega 2560 or Mega ADK
 #define __AVR_ATmega2560__
+#define 
 #define ARDUINO 105
+#define ARDUINO_MAIN
 #define __AVR__
 #define F_CPU 16000000L
 #define __cplusplus
-#define __attribute__(x)
 #define __inline__
 #define __asm__(x)
 #define __extension__
@@ -15,63 +16,65 @@
 #define __inline__
 #define __asm__ 
 #define __volatile__
+
 #define __builtin_va_list
 #define __builtin_va_start
 #define __builtin_va_end
 #define __DOXYGEN__
+#define __attribute__(x)
+#define NOINLINE __attribute__((noinline))
 #define prog_void
 #define PGM_VOID_P int
-#define NOINLINE __attribute__((noinline))
-
+            
 typedef unsigned char byte;
 extern "C" void __cxa_pure_virtual() {;}
 
 
-#include "C:\Program Files\Arduino\hardware\arduino\variants\mega\pins_arduino.h" 
-#include "C:\Program Files\Arduino\hardware\arduino\cores\arduino\arduino.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Marlin.ino"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Configuration.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\ConfigurationStore.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\ConfigurationStore.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Configuration_adv.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\LiquidCrystalRus.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\LiquidCrystalRus.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Marlin.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\MarlinSerial.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\MarlinSerial.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Marlin_main.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Sd2Card.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Sd2Card.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\Sd2PinMap.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdBaseFile.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdBaseFile.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdFatConfig.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdFatStructs.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdFatUtil.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdFatUtil.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdFile.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdFile.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdInfo.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdVolume.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\SdVolume.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\cardreader.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\cardreader.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\fastio.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\language.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\motion_control.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\motion_control.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\pins.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\planner.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\planner.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\speed_lookuptable.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\stepper.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\stepper.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\temperature.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\temperature.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\thermistortables.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\ultralcd.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\ultralcd.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\ultralcd_implementation_hitachi_HD44780.h"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\watchdog.cpp"
-#include "C:\Users\Markus\Documents\Arduino\Marlin-deltabot\Marlin\watchdog.h"
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\mega\pins_arduino.h" 
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Marlin.ino"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Configuration.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\ConfigurationStore.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\ConfigurationStore.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Configuration_adv.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\LiquidCrystalRus.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\LiquidCrystalRus.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Marlin.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\MarlinSerial.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\MarlinSerial.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Marlin_main.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Sd2Card.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Sd2Card.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\Sd2PinMap.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdBaseFile.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdBaseFile.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdFatConfig.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdFatStructs.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdFatUtil.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdFatUtil.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdFile.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdFile.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdInfo.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdVolume.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\SdVolume.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\cardreader.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\cardreader.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\fastio.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\language.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\motion_control.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\motion_control.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\pins.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\planner.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\planner.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\speed_lookuptable.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\stepper.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\stepper.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\temperature.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\temperature.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\thermistortables.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\ultralcd.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\ultralcd.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\ultralcd_implementation_hitachi_HD44780.h"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\watchdog.cpp"
+#include "C:\Users\stoecma2\Desktop\GitHub\deltaSpray\Marlin\watchdog.h"
 #endif
